ceb0e7d66bdd3cb497ce7fd3bef412872bb47d9a
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-24 10:01:13 +0100

    Add Level04/TheThingNamer3000

---
+++ Level04/TheThingNamer3000/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // Item type
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); /* Adjective describing item */
+string c = "of Doom"; /* Suffix "of Doom" */
+string d = "3000"; /* Model number */
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
