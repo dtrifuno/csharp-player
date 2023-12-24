d07b62cab54e0ec2b1154f652fb62de28b699131
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-24 09:48:54 +0100

    Add Level03/TheMakingsOfAProgrammer

---
+++ Level03/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hey, guys!");
+Console.WriteLine("How's it going?");
+Console.WriteLine("Uhh...");
+Console.WriteLine("So I guess we are just staring in silence then?");
+Console.WriteLine("OK...");
