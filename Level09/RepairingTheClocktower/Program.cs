45713da55eaf59b0e11a66f25b0d360181a17717
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 11:47:48 +0100

    Add Level09/RepairingTheClocktower

---
+++ Level09/RepairingTheClocktower/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("What's the current number? ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) {
+    Console.WriteLine("Tick");
+} else {
+    Console.WriteLine("Tock");
+}
