05054505f0a6d9dc1666a436e5b909730bbff2b2
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 12:35:46 +0100

    Add Level09/Watchtower

---
+++ Level09/Watchtower/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enemy's x coordinate: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enemy's y coordinate: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+var direction = "";
+
+if (y < 0) {
+    direction = "south";
+} else if (y > 0) {
+    direction = "north";
+}
+
+if (x < 0) {
+    direction += "west";
+} else if (x > 0) {
+    direction += "east";
+}
+
+if (x == 0 && y == 0) {
+    Console.WriteLine("The enemy is here!");
+} else {
+    Console.WriteLine($"The enemy is to the {direction}!");
+}
