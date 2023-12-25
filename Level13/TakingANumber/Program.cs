4b96d47903ea4d19f5b176b870427e8ea03f955a
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 12:37:40 +0100

    Add Level13/TakingANumber

---
+++ Level13/TakingANumber/Program.cs
@@ -0,0 +1,12 @@
+﻿int AskForANumber(string text) {
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = AskForANumber("What's the current number? ");
+if (number % 2 == 0) {
+    Console.WriteLine("Tick");
+} else {
+    Console.WriteLine("Tock");
+}
