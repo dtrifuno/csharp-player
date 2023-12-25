ee8b3f34e5697f4d03bf9ae5302efe10fc0494a6
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 11:45:52 +0100

    Add Level07/TheFourSistersAndTheDuckbear

---
+++ Level07/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("How many eggs did you gather today?");
+int eggs = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Each sister gets " + eggs / 4 + " eggs, and there are " + eggs % 4 + " eggs left over for the duckbear.");
