c0a027a36432c430f0789e40d49d98022768ba07
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 12:37:09 +0100

    Add Level12/TheReplicatorOfDto

---
+++ Level12/TheReplicatorOfDto/Program.cs
@@ -0,0 +1,29 @@
+﻿var originals = new int[5];
+for (int i = 0; i < 5; i++) {
+    Console.Write($"Enter the {i+1}th item: ");
+    var value = Convert.ToInt32(Console.ReadLine());
+    originals[i] = value;
+}
+
+var copied = new int[5];
+for (int i = 0; i < 5; i++) {
+    copied[i] = originals[i];
+}
+
+Console.Write($"Original: [");
+for (int i = 0; i < 5; i++) {
+    if (i != 0) {
+        Console.Write(", ");
+    }
+    Console.Write(originals[i]);
+}
+Console.WriteLine("]");
+
+Console.Write($"Copied: [");
+for (int i = 0; i < 5; i++) {
+    if (i != 0) {
+        Console.Write(", ");
+    }
+    Console.Write(copied[i]);
+}
+Console.WriteLine("]");
