7e4a626c300e247eb81bc965131d5737a425a07a
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 11:45:32 +0100

    Add Level07/TheTriangleFarmer

---
+++ Level07/TheTriangleFarmer/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("What is the base of the triangle?");
+double baseSize = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("What is the height of the triangle?");
+double height = Convert.ToDouble(Console.ReadLine());
+double area = 0.5 * baseSize * height;
+Console.WriteLine("The area of the triangle is " + area + ".");
