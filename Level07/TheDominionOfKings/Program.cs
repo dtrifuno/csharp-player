56fd010c547851564bf75cbc9dec0dd3ffab1b24
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 11:46:09 +0100

    Add Level07/TheDominionOfKings

---
+++ Level07/TheDominionOfKings/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("How many provinces does His Highness have?");
+int provinces = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("And duchies?");
+int duchies = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("And estates?");
+int estates = Convert.ToInt32(Console.ReadLine());
+
+int score = 6 * provinces + 3 * duchies + estates;
+Console.WriteLine("His Highness's score is " + score + ".");
