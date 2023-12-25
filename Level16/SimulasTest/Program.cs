898a7944851afd7485afd245ba8a709c9011d40e
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 12:50:43 +0100

    Add Level16/SimulasTest

---
+++ Level16/SimulasTest/Program.cs
@@ -0,0 +1,33 @@
+﻿ChestState state = ChestState.Locked;
+while (true) {
+    PrintState(state);
+    Console.Write(" What do you want to do? ");
+    string action = Console.ReadLine().ToLower();
+    if (state == ChestState.Open && action == "close") {
+        state = ChestState.Closed;
+    } else if (state == ChestState.Locked && action == "unlock") {
+        state = ChestState.Closed;
+    } else if (state == ChestState.Closed && action == "lock") {
+        state = ChestState.Locked;
+    } else if (state == ChestState.Closed && action == "open") {
+        state = ChestState.Open;
+    }
+}
+
+void PrintState(ChestState state) {
+    string stateDescription = state switch {
+        ChestState.Open => "open",
+        ChestState.Closed => "closed",
+        ChestState.Locked => "locked"
+    };
+    Console.Write($"The chest is {stateDescription}.");
+}
+
+enum ChestState
+{
+    Open,
+    Closed,
+    Locked,
+    
+}
+
