fb31827208376a4f79390f18b6df7b6c085b6b8a
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 12:36:06 +0100

    Add Level10/BuyingInventory

---
+++ Level10/BuyingInventory/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine("The following items are available:");
+Console.WriteLine("1 - Rope");
+Console.WriteLine("2 - Torches");
+Console.WriteLine("3 - Climbing Equipment");
+Console.WriteLine("4 - Clean Water");
+Console.WriteLine("5 - Machete");
+Console.WriteLine("6 - Canoe");
+Console.WriteLine("7 - Food Supplies");
+Console.Write("What number do you want to see the price of? ");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+string itemName = choice switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies",
+};
+
+int price = choice switch
+{
+    1 => 10,
+    2 => 15,
+    3 => 25,
+    4 => 1,
+    5 => 20,
+    6 => 200,
+    7 => 1
+};
+
+Console.WriteLine($"{itemName} cost {price} gold.");
