f3391fa5d7d167148cc08db03f56fd43753286a8
Darko Trifunovski <dtrifuno@gmail.com>
2023-12-25 12:36:37 +0100

    Add Level11/ThePrototype

---
+++ Level11/ThePrototype/Program.cs
@@ -0,0 +1,21 @@
+﻿int number;
+do {
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    number = Convert.ToInt32(Console.ReadLine());
+} while (number < 0 || number > 100);
+
+Console.Clear();
+
+Console.WriteLine("User 2, guess the number.");
+while (true) {
+    Console.Write("What is your next guess? ");
+    int guess = Convert.ToInt32(Console.ReadLine());
+    if (guess < number) {
+        Console.WriteLine($"{guess} is too low.");
+    } else if (guess > number) {
+        Console.WriteLine($"{guess} is too high.");
+    } else {
+        Console.WriteLine("You guessed the number!");
+        break;
+    }
+} 
